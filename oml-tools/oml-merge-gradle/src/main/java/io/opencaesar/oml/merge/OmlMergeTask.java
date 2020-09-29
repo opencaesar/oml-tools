83295fc3b779cce7a2b8c28bf62737e7833346fe
Nicolas Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2020-09-29 10:59:18 -0700

    Simplified oml-merge. (#14)
    * Simplified oml-merge to handle any combination of 3 kinds of inputs: folders, zips and catalogs.
    Each kind induces a root folder and input files relative to the root folder.
    All the relative file paths must be either unique or have contents with identical checksums.
    
    * some minor fixes
    
    * upgrade version
    
    * The gradle task properties need to be typed by File or Collection<File> so that they can be conveniently configured in a gradle script.
    
    * Copying unique-content files may result in copying the same contents multiple times; hence the copy option for replacing the existing file, if any.
    
    * added a boolean -g parameter to generate output catalog
    
    Co-authored-by: Maged Elaasar <melaasar@gmail.com>

--- oml-tools/oml-merge-gradle/src/main/java/io/opencaesar/oml/merge/OmlMergeTask.java
+++ oml-tools/oml-merge-gradle/src/main/java/io/opencaesar/oml/merge/OmlMergeTask.java
@@ -1,6 +1,8 @@
 package io.opencaesar.oml.merge;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.gradle.api.DefaultTask;
@@ -8,23 +10,46 @@ import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskExecutionException;
 
 public class OmlMergeTask extends DefaultTask {
+
+	public Collection<File> inputZipPaths = null;
 	
-	public List<String> inputCatalogPaths = null;
+	public Collection<File> inputFolderPaths = null;
+	
+	public Collection<File> inputCatalogPaths = null;
     
-    public String outputCatalogPath;
+    public File outputCatalogFolder;
     
+    public boolean generateOutputCatalog;
+
     public boolean debug;
 
     @TaskAction
     public void run() {
     	List<String> args = new ArrayList<String>();
-		for (String inputCatalogPath : inputCatalogPaths) {
-			args.add("-i");
-			args.add(inputCatalogPath);
+    	if (null != inputZipPaths) {
+			for (File inputZipPath : inputZipPaths) {
+				args.add("-z");
+				args.add(inputZipPath.getAbsolutePath());
+			}
 		}
-		if (outputCatalogPath != null) {
+    	if (null != inputFolderPaths) {
+			for (File inputFolderPath : inputFolderPaths) {
+				args.add("-f");
+				args.add(inputFolderPath.getAbsolutePath());
+			}
+		}
+    	if (null != inputCatalogPaths) {
+			for (File inputCatalogPath : inputCatalogPaths) {
+				args.add("-c");
+				args.add(inputCatalogPath.getAbsolutePath());
+			}
+		}
+		if (outputCatalogFolder != null) {
 			args.add("-o");
-			args.add(outputCatalogPath);
+			args.add(outputCatalogFolder.getAbsolutePath());
+		}
+		if (generateOutputCatalog) {
+			args.add("-g");
 		}
 		if (debug) {
 			args.add("-d");
---
+++ oml-tools/oml-merge/src/test/java/OmlMergeTests.java
@@ -0,0 +1,154 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import com.beust.jcommander.JCommander;
+import io.opencaesar.oml.merge.OmlMergeApp;
+import org.junit.*;
+
+public class OmlMergeTests {
+
+    static Path test1_folder1;
+    static Path test1_folder2;
+    static Path test1_output;
+
+    static Path test2_folder1;
+    static Path test2_folder2;
+    static Path test2_output;
+
+    static Path test3_folder2;
+    static Path test3_output;
+
+    static Path test4_folder2;
+    static Path test4_output;
+    
+    @BeforeClass
+    public static void setUp() throws Exception {
+        test1_folder1 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test1-folder1-");
+        test1_folder1.toFile().deleteOnExit();
+        test1_folder1.resolve("A/B").toFile().mkdirs();
+        test1_folder1.resolve("C").toFile().mkdirs();
+
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test1/folder1/A/B/c.oml"), test1_folder1.resolve("A/B/c.oml"));
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test1/folder1/C/d.oml"), test1_folder1.resolve("C/d.oml"));
+
+        test1_folder2 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test1-folder2-");
+        test1_folder2.toFile().deleteOnExit();
+        test1_folder2.resolve("A/B").toFile().mkdirs();
+        test1_folder2.resolve("C").toFile().mkdirs();
+
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test1/folder2/A/B/c.oml"), test1_folder2.resolve("A/B/c.oml"));
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test1/folder2/C/d.oml"), test1_folder2.resolve("C/d.oml"));
+
+        test1_output = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test1-folder12-");
+        test1_output.toFile().deleteOnExit();
+
+        test2_folder1 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test2-folder1-");
+        test2_folder1.toFile().deleteOnExit();
+        test2_folder1.resolve("A/B").toFile().mkdirs();
+        test2_folder1.resolve("C").toFile().mkdirs();
+
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test2/folder1/A/B/c.oml"), test2_folder1.resolve("A/B/c.oml"));
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test2/folder1/C/d.oml"), test2_folder1.resolve("C/d.oml"));
+
+        test2_folder2 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test2-folder2-");
+        test2_folder2.toFile().deleteOnExit();
+        test2_folder2.resolve("A/B").toFile().mkdirs();
+        test2_folder2.resolve("C").toFile().mkdirs();
+
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test2/folder2/A/B/c.oml"), test2_folder2.resolve("A/B/c.oml"));
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test2/folder2/C/d.oml"), test2_folder2.resolve("C/d.oml"));
+
+        test2_output = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test2-folder12-");
+        test2_output.toFile().deleteOnExit();
+
+        test3_folder2 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test3-folder2-");
+        test3_folder2.toFile().deleteOnExit();
+        
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test3/folder2.zip"), test3_folder2.resolve("folder2.zip"));
+
+        test3_output = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test3-folder12-");
+        test3_output.toFile().deleteOnExit();
+        
+        test4_folder2 = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test4-folder2-");
+        test4_folder2.toFile().deleteOnExit();
+
+        Files.copy(OmlMergeTests.class.getResourceAsStream("/test4/folder2.zip"), test4_folder2.resolve("folder2.zip"));
+
+        test4_output = Files.createTempDirectory(Paths.get("/tmp"), "oml-merge-test4-folder12-");
+        test4_output.toFile().deleteOnExit();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        deleteDirectoryRecursively(test1_folder1.toFile());
+        deleteDirectoryRecursively(test1_folder2.toFile());
+        deleteDirectoryRecursively(test2_folder1.toFile());
+        deleteDirectoryRecursively(test2_folder2.toFile());
+        deleteDirectoryRecursively(test3_folder2.toFile());
+        deleteDirectoryRecursively(test4_folder2.toFile());
+    }
+    
+    @Test
+    public void testMergeIdenticalFolders() throws IOException {
+        OmlMergeApp app = new OmlMergeApp();
+        final JCommander builder = JCommander.newBuilder().addObject(app).build();
+        builder.parse("-f", test1_folder1.toFile().getAbsolutePath(), "-f", test1_folder2.toFile().getAbsolutePath(), "-o", test1_output.toFile().getAbsolutePath(), "-g");
+        List<OmlMergeApp.UniqueFile> differences = app.run();
+        Assert.assertTrue(differences.isEmpty());
+        Set<Path> resultPaths = Files.walk(test1_output).collect(Collectors.toSet());
+        Assert.assertTrue(resultPaths.size() == 7);
+    }
+
+    @Test
+    public void testMergeDifferentFolders() throws IOException {
+        OmlMergeApp app = new OmlMergeApp();
+        final JCommander builder = JCommander.newBuilder().addObject(app).build();
+        builder.parse("-f", test2_folder1.toFile().getAbsolutePath(), "-f", test2_folder2.toFile().getAbsolutePath(), "-o", test2_output.toFile().getAbsolutePath(), "-g");
+        List<OmlMergeApp.UniqueFile> differences = app.run();
+        Assert.assertTrue(differences.size() == 1);
+        Set<Path> resultPaths = Files.walk(test2_output).collect(Collectors.toSet());
+        Assert.assertTrue(resultPaths.size() == 7);
+    }
+
+    @Test
+    public void testMergeIdenticalFolderAndZip() throws IOException {
+        OmlMergeApp app = new OmlMergeApp();
+        final JCommander builder = JCommander.newBuilder().addObject(app).build();
+        builder.parse("-f", test1_folder1.toFile().getAbsolutePath(), "-z", test3_folder2.resolve("folder2.zip").toFile().getAbsolutePath(), "-o", test3_output.toFile().getAbsolutePath(), "-g");
+        List<OmlMergeApp.UniqueFile> differences = app.run();
+        Assert.assertTrue(differences.isEmpty());
+        Set<Path> resultPaths = Files.walk(test3_output).collect(Collectors.toSet());
+        Assert.assertTrue(resultPaths.size() == 7);
+    }
+
+    @Test
+    public void testMergeDifferentFolderAndZip() throws IOException {
+        OmlMergeApp app = new OmlMergeApp();
+        final JCommander builder = JCommander.newBuilder().addObject(app).build();
+        builder.parse("-f", test1_folder1.toFile().getAbsolutePath(), "-z", test4_folder2.resolve("folder2.zip").toFile().getAbsolutePath(), "-o", test4_output.toFile().getAbsolutePath(), "-g");
+        List<OmlMergeApp.UniqueFile> differences = app.run();
+        Assert.assertTrue(differences.size() == 1);
+        Set<Path> resultPaths = Files.walk(test4_output).collect(Collectors.toSet());
+        Assert.assertTrue(resultPaths.size() == 7);
+    }
+
+    public static void deleteDirectoryRecursively(File dir) {
+        if (dir.isDirectory()) {
+            File[] files = dir.listFiles();
+            if (files != null && files.length > 0) {
+                for (File aFile : files) {
+                    deleteDirectoryRecursively(aFile);
+                }
+            }
+            dir.delete();
+        } else {
+            dir.delete();
+        }
+    }
+}
